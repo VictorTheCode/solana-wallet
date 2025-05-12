71d29a64b94cd096656d72055694b46bb60b352b
Victor okpaire <vixtorjason@gmail.com>
2025-05-12 21:22:55 +0100

    feat: enabled functionality to ping to confirm transactions

--- src/components/Ping.tsx
+++ src/components/Ping.tsx
@@ -1,7 +1,7 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import * as web3 from "@solana/web3.js";
 import { ArrowRight } from "lucide-react";
-import type { FC } from "react";
+import { useState, type FC } from "react";
 
 const PROGRAM_ID = `ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa`;
 const DATA_ACCOUNT_KEY = `Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod`;
@@ -9,7 +9,12 @@ const DATA_ACCOUNT_KEY = `Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod`;
 const Ping: FC = () => {
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
+  const [sig, setSig] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   const onClick = async () => {
+    setSig(null);
+    setError(null);
     if (!publicKey || !connection) {
       console.error("Wallet not connected or connection not available");
       return;
@@ -36,20 +41,44 @@ const Ping: FC = () => {
       transaction.add(instruction);
 
       const signature = await sendTransaction(transaction, connection);
-      console.log(`Transaction Signature: ${signature}`);
-    } catch (error) {
-      console.error("Error sending transaction:", error);
+      setSig(signature);
+    } catch {
+      const errorMessage = "Failed to send transaction";
+      setError(errorMessage);
     }
   };
   return (
     <div className="flex flex-col md:flex-row justify-center gap-4">
-      <button
-        onClick={onClick}
-        className="px-8 py-3 bg-gradient-to-r from-purple-600 to-blue-600 rounded-lg font-medium hover:from-purple-700 hover:to-blue-700 transition shadow-lg flex items-center justify-center"
-      >
-        Ping!
-        <ArrowRight size={18} className="ml-2" />
-      </button>
+      {!sig && (
+        <button
+          onClick={onClick}
+          className="px-8 py-3 bg-gradient-to-r from-purple-600 to-blue-600 rounded-lg font-medium hover:from-purple-700 hover:to-blue-700 transition shadow-lg flex items-center justify-center"
+        >
+          Ping!
+          <ArrowRight size={18} className="ml-2" />
+        </button>
+      )}
+
+      {sig && (
+        <p className="text-green-400 text-xl">
+          ✅ Transaction sent!
+          <br />
+          <a
+            href={`https://explorer.solana.com/tx/${sig}?cluster=devnet`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:text-green-300"
+          >
+            View on Solana Explorer
+          </a>
+        </p>
+      )}
+
+      {error && (
+        <div className="text-red-500">
+          <p>Error: {error}</p>
+        </div>
+      )}
     </div>
   );
 };
